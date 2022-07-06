a5fa90ad1c2d5e2732a5d0844b63e063f06b99ab
Diogo Freitas <diogo.freitas@hotmail.com>
2022-07-06 19:08:54 -0300

    add HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ConversorModule } from './conversor';
@@ -12,7 +13,8 @@ import { ConversorModule } from './conversor';
   imports: [
     BrowserModule,
     ConversorModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

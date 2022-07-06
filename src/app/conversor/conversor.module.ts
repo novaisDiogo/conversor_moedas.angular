143e0faf4a07dc1d6c2e4a7a47317ee2c623c393
Diogo Freitas <diogo.freitas@hotmail.com>
2022-07-06 19:49:40 -0300

    conversor.component implementation

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
@@ -11,7 +13,9 @@ import { MoedaService, ConversorService } from './services';
     ConversorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent

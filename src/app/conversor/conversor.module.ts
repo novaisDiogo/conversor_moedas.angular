82140e102610a62401a2bcb00d7ad69d7511e2ff
Diogo Freitas <diogo.freitas@hotmail.com>
2022-07-08 10:50:50 -0300

    add pipe dataBr

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -7,6 +7,7 @@ import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
 import { ModalCotacaoComponent } from './utils';
+import { DataBrPipe } from './pipes';
 
 
 
@@ -14,7 +15,8 @@ import { ModalCotacaoComponent } from './utils';
   declarations: [
     ConversorComponent,
     NumeroDirective,
-    ModalCotacaoComponent
+    ModalCotacaoComponent,
+    DataBrPipe
   ],
   imports: [
     CommonModule,
